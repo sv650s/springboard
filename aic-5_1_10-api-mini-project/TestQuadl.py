1c3d3dbed020667927853bd3b3f2aa8fa64e5bd0
Vince Luk <vince.luk@gmail.com>
2019-04-04 11:25:24 -0700

    Updated Jupyter notebook as well as unit tests to use new style assertEquals

--- aic-5_1_10-api-mini-project/TestQuadl.py
+++ aic-5_1_10-api-mini-project/TestQuadl.py
@@ -30,19 +30,19 @@ class TestQuadl(unittest.TestCase):
                          ['2017-01-05', 35.48, 50.00, 50.00, 35.19,
                           None, 54408.0, 1906810.0, None, None, None],
                          ]
-        assert len(
-            unsorted_list) == 4, f"list should be length of 4 but found {len(unsorted_list)}"
+        self.assertEquals(len(unsorted_list), 4, 
+            f"list should be length of 4 but found {len(unsorted_list)}")
 
         # sort by index 3 should give us 04, 02, 03
         sorted_list = self.quadl.sort_data(unsorted_list, 3)
-        assert sorted_list[0][
-            0] == '2017-01-04', f'first should be 04. found {sorted_list[0][0]}'
-        assert sorted_list[1][
-            0] == '2017-01-02', f'second should be 02. found {sorted_list[1][0]}'
-        assert sorted_list[2][
-            0] == '2017-01-03', f'third should be 03. found {sorted_list[2][0]}'
-        assert sorted_list[3][
-            0] == '2017-01-05', f'forth should be 05. found {sorted_list[3][0]}'
+        self.assertEquals(sorted_list[0][0], '2017-01-04', 
+            f'first should be 04. found {sorted_list[0][0]}')
+        self.assertEquals(sorted_list[1][0], '2017-01-02', 
+            f'second should be 02. found {sorted_list[1][0]}')
+        self.assertEquals(sorted_list[2][0], '2017-01-03', 
+            f'third should be 03. found {sorted_list[2][0]}')
+        self.assertEquals(sorted_list[3][0], '2017-01-05', 
+            f'forth should be 05. found {sorted_list[3][0]}')
 
     def test_calculate_median(self):
         """
@@ -51,12 +51,12 @@ class TestQuadl(unittest.TestCase):
         # if odd length, then middle number is returned
         list1 = [[1], [2], [3]]
         median1 = self.quadl.calculate_median(list1, 0)
-        assert median1 == 2, f'median should be 2 but got {median1}'
+        self.assertEquals(median1, 2, f'median should be 2 but got {median1}')
 
         # if even length, then average of middle 2 numbers is returned
         list2 = [[1], [2], [3], [4]]
         median2 = self.quadl.calculate_median(list2, 0)
-        assert median2 == 2.50, f'median should be 2.50 but got {median2}'
+        self.assertEquals(median2, 2.50, f'median should be 2.50 but got {median2}')
 
     def test_calculate_stats(self):
         """
@@ -68,21 +68,21 @@ class TestQuadl(unittest.TestCase):
         stats = self.quadl.calculate_stats()
 
         min_open_price = stats['min_open_price']
-        assert stats['min_open_price'] == self.df.Open.min(), \
-            f"min open price should be {self.df.Open.min()} but got {min_open_price}"
+        self.assertEquals(stats['min_open_price'], self.df.Open.min(), \
+            f"min open price should be {self.df.Open.min()} but got {min_open_price}")
         max_open_price = stats['max_open_price']
-        assert stats['max_open_price'] == self.df.Open.max(), \
-            f"max open price should be {self.df.Open.max()} but got {max_open_price}"
+        self.assertEquals(stats['max_open_price'], self.df.Open.max(), \
+            f"max open price should be {self.df.Open.max()} but got {max_open_price}")
         max_daily_change = stats['max_daily_change']
-        assert max_daily_change == round(self.df.daily_change.max(), 2), \
-            f"max daily_change should be {round(self.df.daily_change.max(), 2)} but got {max_daily_change}"
+        self.assertEquals(max_daily_change, round(self.df.daily_change.max(), 2), \
+            f"max daily_change should be {round(self.df.daily_change.max(), 2)} but got {max_daily_change}")
         average_trading_volume = stats['average_trading_volume']
-        assert average_trading_volume == round(self.df['Traded Volume'].mean(), 2), \
-            f"average traded volumen should be {round(self.df['Traded Volume'].mean(), 2)} but got {average_trading_volume}"
+        self.assertEquals(average_trading_volume, round(self.df['Traded Volume'].mean(), 2), \
+            f"average traded volumen should be {round(self.df['Traded Volume'].mean(), 2)} but got {average_trading_volume}")
 
         # TODO: check max 2 day change
         self.df = self.df.assign(two_day_change=self.df['Close'].rolling(
-            window=2, center=False).apply(lambda x: round(x[1] - x[0], 2)))
+            window=2, center=False).apply(lambda x: round(x[1] - x[0], 2), raw=True))
         self.assertEquals(stats['max_two_day_change'], self.df.two_day_change.max(),
                           f"max 2 day change should be {self.df.two_day_change.max()} got {stats['max_two_day_change']}")
 
